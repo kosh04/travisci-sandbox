45f57f520d487d5e53e202f61355746653411f50
kosh04 <shigeru.kb@gmail.com>
2019-05-26 14:48:54 +0900

    add test gnutls-cli

---
+++ gnutls-cli-test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -eu
+
+https_get() {
+    host=${1}; path=${2}
+    printf "GET %s HTTP/1.0\r\n\r\n" "${path}" | gnutls-cli --port=https "${host}"
+}
+
+https_get "httpbin.org" "/get"
+
+# https://travis-ci.org/kosh04/emacs-wandbox/jobs/534401766
+https_get "wandbox.fetus.jp" "/404"
+https_get "wandbox.fetus.jp" "/api/list.json"
